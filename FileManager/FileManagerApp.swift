deedae5058661e8463f1bb2d4fe5a2d2d32ee39a
Kirill Panichkin <unbearable.panic@gmail.com>
2026-02-23 21:40:57 +0400

    Initial Commit

---
+++ FileManager/FileManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerApp.swift
+//  FileManager
+//
+//  Created by Кирилл Паничкин on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
